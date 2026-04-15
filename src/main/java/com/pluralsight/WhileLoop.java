7941f8a5dc6f077e4294a1f324e43c56ecf0eef2
Fernando <f.miranda25d@gmail.com>
2026-04-15 11:24:29 -0500

    First iteration of for loop runs

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -2,16 +2,17 @@ package com.pluralsight;
 
 public class WhileLoop {
     public static void main(String[] args) {
-        int count1 = 1;
 
+        int count1 = 1;
         while (count1 <= 5) {
             System.out.println("I love Java");
             count1 = count1 + 1;
         }
         main2();
-
     }
+
     public static void main2() {
+
         int count2 = 1;
         do {
             System.out.println("I love Java");
