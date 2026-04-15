b3ebd20b29651e48c27c3769cca067d573912f0f
Fernando <f.miranda25d@gmail.com>
2026-04-15 11:15:48 -0500

    Third commit, do/while runs properly

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        int count1 = 1;
+
+        while (count1 <= 5) {
+            System.out.println("I love Java");
+            count1 = count1 + 1;
+        }
+        main2();
+
+    }
+    public static void main2() {
+        int count2 = 1;
+        do {
+            System.out.println("I love Java");
+            count2++;
+        } while (count2 <= 5);
+    }
 }
